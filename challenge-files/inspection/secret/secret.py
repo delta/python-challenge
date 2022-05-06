05c30447ad6285c2ef4fafe3cd8e9d175c370214
Shreyas Krishnakumar <shreyaskumar1035@gmail.com>
2022-05-06 23:36:04 +0530

    feat(challenges-web): Add inspect and pyc challenges

---
+++ challenge-files/inspection/secret/secret.py
@@ -0,0 +1,5 @@
+# Great, here's the solution: alAX95eC23CTwk6aiKG1
+
+def GJvD0KRbJCdRhmoLX2cY():
+    # Hi there, now find a top level comment in the file in which this function was declared!
+    print('Great, now find the source of this function. The next clue is in a comment in this function ')
