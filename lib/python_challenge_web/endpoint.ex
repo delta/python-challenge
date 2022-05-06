ac624ccbc284908aa51fde9d9ed6a8bf6fd99db2
Shreyas Krishnakumar <shreyaskumar1035@gmail.com>
2022-05-06 18:37:50 +0530

    feat(challenges-web): Add csv, compression and numpy challenge

--- lib/python_challenge_web/endpoint.ex
+++ lib/python_challenge_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule PythonChallengeWeb.Endpoint do
     at: "/",
     from: :python_challenge,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets fonts images 8 10 favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
