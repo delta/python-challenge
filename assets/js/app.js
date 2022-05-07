31db1452f71ff0854eb61c5810a1883c35ff2931
kelpikz <ajithaprasad96@gmail.com>
2022-05-07 20:04:00 +0530

    feat(style): add styling for challenge page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,15 @@
 // We import the CSS which is extracted to its own file by esbuild.
 // Remove this line if you add a your own CSS build pipeline (e.g postcss).
-import "../css/app.css"
+import "../css/app.css";
+
+const params = new Proxy(new URLSearchParams(window.location.search), {
+	get: (searchParams, prop) => searchParams.get(prop),
+});
+if (params.solution == "incorrect") {
+	console.log("hello");
+	const x = document.querySelector("div#question-panel p.hidden.incorrect");
+	if (x) x.style.display = "block";
+}
 
 // If you want to use Phoenix channels, run `mix help phx.gen.channel`
 // to get started and then uncomment the line below.
